19196619fa9877ca0d4687b381dcba6c1e6d5b82
byhj <476652422@qq.com>
2015-11-21 09:33:36 +0800

    mvc

--- 02_MVC/GeoQuiz/app/src/main/java/com/example/byhj/geoquiz/QuizActivity.java
+++ 02_MVC/GeoQuiz/app/src/main/java/com/example/byhj/geoquiz/QuizActivity.java
@@ -6,36 +6,113 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private ImageButton mPrevButton;
+    private ImageButton mNextButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[]
+            {
+                    new Question(R.string.question_oceans, true),
+                    new Question(R.string.question_mideast, false),
+                    new Question(R.string.question_americas, true),
+            };
+
+    private int mCurrentIndex = 0;
+
+    private void updateQuestion()
+    {
+        int question = mQuestionBank[mCurrentIndex].getTextResid();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue)
+    {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue)
+        {
+            messageResId = R.string.correct_toast;
+        }
+        else
+        {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+                                                 @Override
+                                                 public void onClick(View v) {
+                                                     mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                                                     updateQuestion();
+                                                 }
+                                             }
+        );
+
         //True Click listener
         mTrueButton = (Button) findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
+        mTrueButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            public void onClick(View v)
+            {
+                checkAnswer(true);
             }
         });
 
        //False Click listener
         mFalseButton = (Button) findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
+        mFalseButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            public void onClick(View v)
+            {
+
+                checkAnswer(false);
             }
         });
 
+        //next prev click
+        mPrevButton = (ImageButton)findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (mCurrentIndex == 0)
+                    mCurrentIndex = mQuestionBank.length - 1;
+                else
+                    mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        //next button click
+        mNextButton = (ImageButton)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
     }
 
     @Override
